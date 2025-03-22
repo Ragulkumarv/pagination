9ff5730dee2af29064f29c30ff59fae4177f681a
Ragul <ragulkumarv333@gmail.com>
2025-03-22 16:54:02 +0530

    added pagination changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+import Pagination from "./components/Pagination";
+
+const App = () => {
+  const items = Array.from({ length: 50 }).map(
+    (_, index) => `Item ${index + 1}`
   );
-}
+
+  const itemsPerPage = 5;
+  return (
+    <section className="">
+      <h1>Pagination Component</h1>
+      <Pagination items={items} itemsPerPage={itemsPerPage} />
+    </section>
+  );
+};
 
 export default App;
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const Pagination = ({ items, itemsPerPage }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(items.length / itemsPerPage);
+
+  const currentItems = items.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handlePrev = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
+  return (
+    <section className="pagination__wrapper">
+      <ul className="pagination__items">
+        {currentItems.map((item, index) => (
+          <li key={`Item-${index}`}>{item}</li>
+        ))}
+      </ul>
+      <section className="pagination__items--list">
+        <button
+          className="pagination__page-btn"
+          onClick={() => handlePrev()}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+
+        {[...Array(totalPages)].map((_, index) => (
+          <button
+            key={`btn-${index}`}
+            className={`pagination__page-btn ${
+              currentPage === index + 1 ? "active" : ""
+            }`}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </button>
+        ))}
+
+        <button
+          className="pagination__page-btn"
+          onClick={() => handleNext()}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </section>
+    </section>
+  );
+};
+
+export default Pagination;
